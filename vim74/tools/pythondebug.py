e23f357f67a91e4edafcac4fa085bb97a0e162e7
chooaya <chooaya@gmail.com>
2016-07-25 12:34:01 +0900

    f

--- vim74/tools/pythondebug.py
+++ vim74/tools/pythondebug.py
@@ -2,23 +2,17 @@
 # -*- coding: utf-8 -*-
 # pythondebug.py
 import os
-import StringIO
 import sys
 sys.path.append(os.path.join(os.path.realpath( os.path.join(os.getcwd(), os.path.dirname(__file__))), 'pydbgp','bin'))
 import pydbgp
 
-
-#output = StringIO.StringIO()
-#sys.stdout = output
-print('Loading function')
 param = sys.argv
 if len(param) >= 3:
     virargv = ['', '-d',str(param[1]),param[2]]
     try:
         ret = pydbgp.mainc(virargv)
-        #sys.stdout = sys.__stdout__
         if ret == 1:
-            print(ret)
+            os.system("python "+param[2])
     except Exception as e:
         pass
 
